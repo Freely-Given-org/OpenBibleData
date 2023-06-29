f48156dcdccb9736993e580d457e7d4631fcf70a
Robert Hunt <Freely.Given.org@gmail.com>
2023-06-29 20:53:15 +0100

    Updates

--- createPages/usfm.py
+++ createPages/usfm.py
@@ -870,17 +870,17 @@ def formatUSFMText( versionAbbreviation:str, refTuple:tuple, segmentType:str, us
     for charMarker in BibleOrgSysGlobals.USFMAllExpandedCharacterMarkers + ['untr']:
         html = html.replace( f'\\{charMarker} ', f'<span class="{charMarker}">' ).replace( f'\\{charMarker}*', '</span>' )
 
-    # Append "untranslated" to titles/popup-boxes for untranslated words
-    count = 0
-    searchStartIndex = 0
-    for _safetyCount in range( 1000 ):
-        ix = html.find( '<span class="untr"><span title="', searchStartIndex )
-        if ix == -1: break # all done
-        ixEnd = html.index( '" href=', ix+32 )
-        html = f'{html[:ixEnd]} (untranslated){html[ixEnd:]}'
-        count += 1
-        searchStartIndex = ixEnd + 5
-    else: need_to_increase_loop_count_for_untranslated_words
+    if 'OET' in versionAbbreviation: # Append "untranslated" to titles/popup-boxes for untranslated words in OET-LV
+        # count = 0
+        searchStartIndex = 0
+        for _safetyCount in range( 1000 ):
+            ix = html.find( '<span class="untr"><a title="', searchStartIndex )
+            if ix == -1: break # all done
+            ixEnd = html.index( '" href=', ix+29 )
+            html = f'{html[:ixEnd]} (untranslated){html[ixEnd:]}'
+            # count += 1
+            searchStartIndex = ixEnd + 5
+        else: need_to_increase_loop_count_for_untranslated_words
 
     # Final checking
     if versionAbbreviation not in ('UST','ULT'): # uW stuff has too many USFM encoding errors and inconsistencies
